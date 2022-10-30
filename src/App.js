d085762b1615990c732227ad86b956cc018cb34e
demitraps <109766095+demitraps@users.noreply.github.com>
2022-10-30 18:12:04 +0200

    Added footer with Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        <a
+          href="https://github.com/demitraps/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Demitra Ps
+      </footer>
+    </div>
+  );
 }
